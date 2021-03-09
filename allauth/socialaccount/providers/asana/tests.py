e55db251df211f53b3ea79a6ba9f5dfcc2cc3fa2
diegodirob <64582340+diegodirob@users.noreply.github.com>
2021-03-09 12:17:44 +0100

    Update tests.py
    Updated id to gid

--- allauth/socialaccount/providers/asana/tests.py
+++ allauth/socialaccount/providers/asana/tests.py
@@ -10,7 +10,9 @@ class AsanaTests(create_oauth2_tests(registry.by_id(AsanaProvider.id))):
         return MockedResponse(
             200,
             """
-{"data": {"photo": null, "workspaces": [{"id": 31337, "name": "example.com"},
-{"id": 3133777, "name": "Personal Projects"}], "email": "test@example.com",
-"name": "Test Name", "id": 43748387}}""",
+{"data": {"photo": null, "workspaces": [{"gid": 31337, "name": "example.com"},
+{"gid": 3133777, "name": "Personal Projects"}], "email": "test@example.com",
+"name": "Test Name", "gid": 43748387}}""",
         )
+    
+ 
